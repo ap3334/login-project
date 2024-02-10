8ff7f400abc66fc8281ac23e0402be6da4caf3d0
ap3334 <chwil2989@naver.com>
2024-02-10 15:53:00 +0900

    user repository 생성

---
+++ src/main/java/com/jung/login/user/userRepository.java
@@ -0,0 +1,8 @@
+package com.jung.login.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface userRepository extends JpaRepository<User, Long> {
+}
